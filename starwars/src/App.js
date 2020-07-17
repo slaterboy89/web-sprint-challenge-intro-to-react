1e8bec2ca8524420cfd1627829e7d6dddef674d2
Leonard <slaterboy89@gmail.com>
2020-07-17 13:59:13 -0500

    updated app.js

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 import './App.css';
+import Character from './components/Character'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,7 +10,35 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [characters, setCharacters] = useState([])
+  const [currentCharacterID, setCurrentCharacterID] = useState(null)
 
+  const openDetails = id => {
+    setCurrentCharacterID(id)
+  }
+
+  const closeDetails = () => {
+    setCurrentCharacterID(null)
+  }
+
+  useEffect(() => {
+    axios.get( 'https://swapi.dev/api/people/')
+      .then(res => {
+        setCharacters(res.data)
+        console.log(res)
+      })
+      .catch(err => {
+        console.log('error')
+      })
+  }, [])
+  const myCharacter = ({info}) => (
+    <div className = 'character'>
+      {info.name}
+      <button onclick = {(event) => openDetails(info.id)}>
+        See Details
+        </button>
+    </div>
+  )
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
