6600821c22c3e2bc1b01df6e01a13591db1a44c0
Leonard <slaterboy89@gmail.com>
2020-07-17 16:50:21 -0500

    finished assignment

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import './App.css';
+import './App.css'
 import Character from './components/Character'
 
 const App = () => {
@@ -11,39 +11,35 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
   const [characters, setCharacters] = useState([])
-  const [currentCharacterID, setCurrentCharacterID] = useState(null)
+  // const [currentCharacterID, setCurrentCharacterID] = useState(null)
 
-  const openDetails = id => {
-    setCurrentCharacterID(id)
-  }
+  // const openDetails = id => {
+  //   setCurrentCharacterID(id)
+  // }
 
-  const closeDetails = () => {
-    setCurrentCharacterID(null)
-  }
+  // const closeDetails = () => {
+  //   setCurrentCharacterID(null)
+  // }
 
   useEffect(() => {
-    axios.get( 'https://swapi.dev/api/people/')
-      .then(res => {
-        setCharacters(res.data)
+    axios.get('https://swapi.dev/api/people/')
+    .then(res => {
         console.log(res)
+        setCharacters(res.data.results)
       })
-      .catch(err => {
-        console.log('error')
-      })
+    .catch(err => console.log(err))
   }, [])
-  const myCharacter = ({info}) => (
-    <div className = 'character'>
-      {info.name}
-      <button onclick = {(event) => openDetails(info.id)}>
-        See Details
-        </button>
+
+  return (
+    <div className='App'>
+      <h1 className='Header'>Characters</h1>
+      {characters.map((fr, index) => {
+        return(
+          <Character key={index} name={fr.name} gender={fr.gender} height={fr.height} eye_color={fr.eye_color} />
+        )
+      })}
     </div>
   )
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
 }
 
 export default App;
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,2 +1,27 @@
 // Write your Character component here
+import React from 'react'
+// import axios from 'axios'
+import styled from 'styled-components'
 
+const StyledCharacter = styled.div`
+border: 2px solid green;
+margin-left: 20%;
+margin-right:20%;
+h2, p{
+    color: white;
+    font-family: hammerkeys
+}
+
+`
+export default function Character(props) {
+    
+return (
+    <StyledCharacter className='App'>
+        <h2>{props.name} Details:</h2>    
+        <p>Name: {props.name}</p>
+        <p>Gender: {props.gender}</p>
+        <p>Height: {props.height}</p>
+        <p>Eye Color: {props.eye_color}</p>
+  </StyledCharacter>
+)
+}
